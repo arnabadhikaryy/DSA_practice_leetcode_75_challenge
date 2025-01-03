3c52786ba976fe57badf481fd32787cf785f5ae4
arnabadhikaryy <139059216+arnabadhikaryy@users.noreply.github.com>
2025-01-03 18:23:13 +0530

    Kids With the Greatest Number of Candies

---
+++ 75leetcode3.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+
+
+var kidsWithCandies = function(candies, extraCandies) {
+    let result = [];
+    let max = Math.max(...candies);
+
+    candies.map((currentValue, index)=>{
+        if(currentValue+extraCandies >= max){
+            result.push(true)
+        }
+        else{
+            result.push(false)
+        }
+    })
+
+    return result;
+};
+
+candies = [4,2,1,1,2]; extraCandies = 1
+console.log(kidsWithCandies(candies,extraCandies))
